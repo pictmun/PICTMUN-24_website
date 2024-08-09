a40f776b9542f267e770e6622e2df1b57f715601
Hardik Gujrathi <136152739+TheMercury1229@users.noreply.github.com>
2024-08-09 21:14:38 +0530

    Completed all the events page uploads

---
+++ components/Events/Card.tsx
@@ -0,0 +1,33 @@
+
+interface CardProps {
+  right?: boolean;
+  title: string;
+  description: string;
+  bgImage: string;
+}
+
+const Card: React.FC<CardProps> = ({ right, title, description, bgImage }) => {
+  return (
+    <div className="mt-7">
+      <div
+        className={`relative  w-[90%] md:w-[55%] mx-auto ${
+          right ? 'md:ml-auto md:mr-0' : 'md:mr-auto md:ml-0'
+        } h-[450px]`}
+        style={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        {/* Content Overlay */}
+        <div className="absolute inset-0 flex flex-col justify-center p-5 md:p-12 text-midblue bg-white bg-opacity-80">
+          <h2 className="text-center text-3xl font-bold my-5">{title}</h2>
+          <p className="text-left text-lg leading-5">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
