f9621345140800fc8a33d5630fa97fad2c1dc79c
anjali <salway.anjali126@gmail.com>
2024-08-10 19:27:13 +0530

    khatam bhai

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,6 +10,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        slideInLeft: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      slideInRight: {
+        '0%': { transform: 'translateX(100%)' },
+        '100%': { transform: 'translateX(0)' },
+      },
+      animation: {
+        slideInLeft: 'slideInLeft 0.5s ease-out forwards',
+         slideInRight: 'slideInRight 1.5s ease-out forwards',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
