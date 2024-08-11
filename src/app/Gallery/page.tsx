3743c5941b137c97def33083aeb2f3ae246c1f12
yupcoding1 <mdhuzefabbasi@gmail.com>
2024-08-11 12:33:31 +0530

    final touches

--- src/app/Gallery/page.tsx
+++ src/app/Gallery/page.tsx
@@ -4,13 +4,13 @@ import Gallery from '../../../components/Gallery/Card';
 import { useState } from 'react';
 
 export default function Page() {
-    const [hoveredIndex, setHoveredIndex] = useState(null);
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
     return (
         <div className="w-full mx-auto bg-darkblue">
             <h1 className="text-4xl font-bold text-white pt-5 relative">Gallery</h1>
             <h2 className="text-4xl text-white font-bold relative text-center py-12">Gallery</h2>
-            <div className="w-[80%] mx-auto pl-11">
+            <div className="w-[80%] mx-auto ">
                 <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-10 gap-y-20 pb-11 place-content-center px-10">
                     {[...Array(9)].map((_, index) => (
                         <div 
