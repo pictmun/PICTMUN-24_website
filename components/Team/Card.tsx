047de1e417697eb307d4999fff41508187ba420d
yupcoding1 <mdhuzefabbasi@gmail.com>
2024-08-11 10:43:17 +0530

    responsive team

--- components/Team/Card.tsx
+++ components/Team/Card.tsx
@@ -10,10 +10,10 @@ interface CardProps {
   const TeamMember: React.FC<CardProps> = ({ name, designation, image }) => {
     return (
       <div className="team-member">
-        <div className="max-w-[250px] max-h-[250px] ">
+        <div className=" max-w-[200px] max-h-[200px] md:max-w-[250px] md:max-h-[250px]">
           <img className="w-full h-full rounded-full" src={image} alt={name} />
         </div>
-        <div className="info mx-auto text-center py-[15px] text-3xl text-white">
+        <div className="info mx-auto text-center py-[15px] lg:text-3xl text-lg md:text-xl text-white">
           <h3 className=''>{name}</h3>
           <p className='pt-1'>{designation}</p>
         </div>
--- src/app/Team/page.tsx
+++ src/app/Team/page.tsx
@@ -14,11 +14,11 @@ export default function Page() {
 
       <h1 className=" text-4xl font-bold text-white relative  ">Meet the Team</h1><br></br>
       <h2 className="  text-4xl text-white  relative  py-12 " >The Secretariat</h2>
-      <div className="grid grid-cols-2 gap-20 pb-11 align-baseline">
+      <div className="grid grid-cols-2 gap-5 md:gap-20 pb-11 align-baseline">
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
       </div>
-      <div className="grid grid-cols-3 gap-16">
+      <div className="grid grid-cols-3 gap-5 md:gap-16">
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
@@ -29,19 +29,13 @@ export default function Page() {
 
       
       <h2 className="  text-4xl text-white  relative  py-20 ">Under Secretary General</h2>
-      <div className="grid grid-cols-2 gap-36 pb-11 align-baseline">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-20 md:gap-36 pb-11 align-baseline">
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
-      </div>
-      <div className="grid grid-cols-2 gap-36 pb-11 align-baseline">
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
-      </div>
-      <div className="grid grid-cols-2 gap-36 pb-11 align-baseline">
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
-      </div>
-      <div className="grid grid-cols-2 gap-36 pb-11 align-baseline">
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
       <TeamMember  name={'Anshika Singh'} designation={"USG Design"} image={"/images/Anshika_Singh.jpg"}/>
       </div>
