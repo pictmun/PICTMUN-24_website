e0009f148186d4fb51af24a09757e4cb6405affe
pictmun <70532999+pictmun@users.noreply.github.com>
2024-12-25 23:16:26 +0530

    Update page.tsx add link tree for register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
           <h1 className="text-2xl lg:text-2xl text-zinc-400">Think  Discuss  Prosper</h1>
           <div className="h-[25px]"></div>
           <div>
-            <Link href="/">
+            <Link href="https://linktr.ee/pictmun">
             <button className="px-[55px] py-[12px] text-md text-yellow-300 rounded-md hover:scale-105 transition-all duration-300 bg-zinc-950 bg-opacity-60 md:mr-[12.5px] border-[1.5px] border-solid border-zinc-950 border-opacity-100 shadow-lg">Register</button>
             </Link>
             <div className='md:hidden h-[15px]' />
